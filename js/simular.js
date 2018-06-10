3939895ce3444c52336f2b3149cf71954533a5ce
Filipe Voges <filipe.vogesh@gmail.com>
2018-06-10 10:26:01 -0300

    Adicionado mais elementos

---
+++ js/simular.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+
+    // Seções
+    var secTDef = $('#sec-t-def');
+    var secTNDef = $('#sec-t-n-def');
+
+    // Opções
+    var tDef = $('#t-def');
+    var tNDef = $('#t-n-def');
+
+    var sInst = $('#s-inst');
+    var sFast = $('#s-fast');
+    var sLong = $('#s-long');
+    var sNDef = $('#s-n-def');
+
+    // Seção Ocultas por padrão
+    secTDef.hide();
+    secTNDef.hide();
+
+    // Tratamento de eventos
+    tDef.click(function(){
+        secTNDef.hide();
+        secTDef.fadeIn();
+        secTDef.slideDown();
+    });
+    tNDef.click(function(){
+        secTDef.hide();
+        secTNDef.fadeIn();
+        secTNDef.slideDown();
+    });
+
+});
