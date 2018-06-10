7d89f667e27a9011223069e0ddb55afc5f267e69
Filipe Voges <filipe.vogesh@gmail.com>
2018-06-10 14:33:47 -0300

    Finalizado a tela de simulação e inicio

--- js/simular.js
+++ js/simular.js
@@ -3,6 +3,7 @@ $(document).ready(function(){
     // Seções
     var secTDef = $('#sec-t-def');
     var secTNDef = $('#sec-t-n-def');
+    var results = $('#result');
 
     // Opções
     var tDef = $('#t-def');
@@ -13,20 +14,46 @@ $(document).ready(function(){
     var sLong = $('#s-long');
     var sNDef = $('#s-n-def');
 
+    var tSel = $('.titulo');
+
     // Seção Ocultas por padrão
-    // secTDef.hide();
+    secTDef.hide();
     secTNDef.hide();
+    results.hide();
 
     // Tratamento de eventos
     tDef.click(function(){
         secTNDef.hide();
+        results.hide();
         secTDef.fadeIn();
         secTDef.slideDown();
     });
     tNDef.click(function(){
         secTDef.hide();
+        results.hide();
         secTNDef.fadeIn();
         secTNDef.slideDown();
+    });
+
+    tSel.click(function(){
+        results.fadeIn();
+        results.slideDown();
+    });
+    sInst.click(function(){
+        results.fadeIn();
+        results.slideDown();
+    });
+    sFast.click(function(){
+        results.fadeIn();
+        results.slideDown();
+    });
+    sLong.click(function(){
+        results.fadeIn();
+        results.slideDown();
+    });
+    sNDef.click(function(){
+        results.fadeIn();
+        results.slideDown();
     });
 
 });
